65ef073f129b641dd8a10b45b65688a6d7d8a671
Shrey Sambhwani <shrey.sambhwani@gobio.com>
2021-08-03 15:33:56 -0700

    Second Commit

--- src/main/java/com/shreysambhwani/fleetapp/models/User.java
+++ src/main/java/com/shreysambhwani/fleetapp/models/User.java
@@ -17,7 +17,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class User {
 		
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +24,23 @@ public class User {
 	private int id;
 	private String username;
 	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
--- src/main/java/com/shreysambhwani/fleetapp/services/UserService.java
+++ src/main/java/com/shreysambhwani/fleetapp/services/UserService.java
@@ -1,9 +1,7 @@
 package com.shreysambhwani.fleetapp.services;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shreysambhwani.fleetapp.models.User;
@@ -12,26 +10,14 @@ import com.shreysambhwani.fleetapp.repositories.UserRepository;
 @Service
 public class UserService {
 	
-	@Autowired
-	private UserRepository userRepository;
+	@Autowired private BCryptPasswordEncoder encoder;
 	
-	//Get All Users
-	public List<User> findAll(){
-		return userRepository.findAll();
-	}	
-	
-	//Get User By Id
-	public Optional<User> findById(int id) {
-		return userRepository.findById(id);
-	}	
-	
-	//Delete User
-	public void delete(int id) {
-		userRepository.deleteById(id);
-	}
-	
-	//Update User
+	@Autowired private UserRepository userRepository;
+
 	public void save(User user) {
+		
+		user.setPassword(encoder.encode(user.getPassword()));
+		
 		userRepository.save(user);
 	}
 
